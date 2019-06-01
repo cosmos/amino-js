a3835ed042f300cd7b0f2351a57dd3b7d2ac77de
Jordan Sexton <jordan@jordansexton.com>
2019-05-31 20:52:21 -0500

    just export unwrapped functions

--- src/meta.ts
+++ src/meta.ts
@@ -1,21 +1,7 @@
-import * as Amino from '../lib/Amino';
-
-export function decodeDisambPrefixBytes (bytes: Uint8Array): [Uint8Array | null, Uint8Array | null, number] {
-    return Amino.decodeDisambPrefixBytes(bytes);
-}
-
-export function nameToDisfix (name: string): [Uint8Array, Uint8Array] {
-    return Amino.nameToDisfix(name);
-}
-
-export function byteSliceSize (bytes: Uint8Array): number {
-    return Amino.byteSliceSize(bytes);
-}
-
-export function uvarintSize (uvarint: number): number {
-    return Amino.uvarintSize(uvarint);
-}
-
-export function varintSize (varint: number): number {
-    return Amino.varintSize(varint);
-}
+export {
+    decodeDisambPrefixBytes,
+    nameToDisfix,
+    byteSliceSize,
+    uvarintSize,
+    varintSize
+} from '../lib/Amino';
