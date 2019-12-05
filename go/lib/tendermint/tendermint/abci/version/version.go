63d37ef324cf0a20594b1925a7461c321822d47b
Jordan Sexton <jordan@jordansexton.com>
2019-12-04 21:46:35 -0600

    remove misc

--- go/lib/tendermint/tendermint/abci/version/version.go
+++
@@ -1,9 +0,0 @@
-package version
-
-import (
-	"github.com/cosmos/amino-js/go/lib/tendermint/tendermint/version"
-)
-
-// TODO: eliminate this after some version refactor
-
-const Version = version.ABCIVersion
